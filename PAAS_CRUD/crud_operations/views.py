fd3a6fefd06721b3e45b61139954eb67fd7d82cb
Aniruddh M <animaruthesh@gmail.com>
2020-11-19 23:17:15 +0530

    Added RETRIEVE DELETE and UPDATE

--- PAAS_CRUD/crud_operations/views.py
+++ PAAS_CRUD/crud_operations/views.py
@@ -7,11 +7,9 @@ from django.contrib import messages
 def Create(request):
     print("Create called")
     if request.method == "POST":
-        print(request.POST.keys())
         Name = request.POST['Name']
         USN = request.POST['USN']
         Branch = request.POST['Branch']
-        print(Name, USN, Branch)
 
         if Student.objects.filter(pk=USN).exists():
             messages.info(request, "There already exists a student with the USN {}".format(USN))
@@ -25,13 +23,53 @@ def Create(request):
         return render(request, 'crud_operations/crud_create.html')
 
 def Update(request):
-    return 
+    if request.method == "POST":
+        USN = request.POST['USN']
+        New_Branch = request.POST['Branch']
+        if not Student.objects.filter(pk=USN).exists():
+            messages.info(request, "No student with USN {} exists in the database".format(USN))
+        else:
+            try:
+                student = Student.objects.get(pk=USN)
+                student.Branch = New_Branch
+                student.save()
+                messages.info(request, "Updated successfully")
+            except:
+                messages.info(request, "Could not find the student specified")            
+
+        return render(request, 'crud_operations/crud_update.html')
+    else:
+        return render(request, 'crud_operations/crud_update.html') 
 
 def Retrieve(request):
-    return 
+    if request.method == "POST":
+        print(request.POST.keys())
+        USN = request.POST['USN']
+
+        if Student.objects.filter(pk=USN).exists():
+            messages.info(request, "There exists a student with the USN {}".format(USN))
+        else:
+            messages.info(request, "No student with USN {} exists in the database".format(USN))
+
+        return render(request, 'crud_operations/crud_retrieve.html')
+    else:
+        return render(request, 'crud_operations/crud_retrieve.html') 
 
 def Delete(request):
-    return 
+    if request.method == "POST":
+        print(request.POST.keys())
+        USN = request.POST['USN']
+
+        if Student.objects.filter(pk=USN).exists():
+            temp = Student.objects.get(pk=USN)
+            temp.delete()
+            messages.info(request, "Deleted successfully")
+        else:
+            messages.info(request, "No student with USN {} exists in the database".format(USN))
+
+        return render(request, 'crud_operations/crud_delete.html')
+    else:
+        return render(request, 'crud_operations/crud_delete.html') 
     
 def index(request):
     return HttpResponse("Crud home page")
