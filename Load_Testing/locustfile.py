c4b2cd110030a1b3196bd96ac6793793cf0e8927
Aniruddh M <animaruthesh@gmail.com>
2020-11-21 19:59:43 +0530

    Added load testing for POST requests

--- Load_Testing/locustfile.py
+++ Load_Testing/locustfile.py
@@ -1,12 +1,19 @@
 from locust import TaskSet, task, HttpUser
 
 class UserBehaviour(TaskSet):
-    @task
-    def launch_url(self):
-        # GET request
-        self.client.get("/crud/Retrieve")
-        print("Got a webpage response")
+    # @task
+    # def launch_url(self):
+    #     # GET request
+    #     self.client.get("/crud/Retrieve")
+    #     print("Got a webpage response")
     
+    @task
+    def post_retrieve(self):
+        # POST request on the RETRIEVE page
+        response = self.client.get("/Retrieve")
+        csrftoken = response.cookies['csrftoken']
+        self.client.post("/Retrieve", {"USN": "1RV17EC011"}, headers={"X-CSRFToken": csrftoken})
+        print("Posted a request")
 
 class User(HttpUser):
     tasks = [UserBehaviour]
