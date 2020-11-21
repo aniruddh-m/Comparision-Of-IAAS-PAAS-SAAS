74cebf63285ad928b47d06a3d306a413083be345
Aniruddh M <animaruthesh@gmail.com>
2020-11-21 14:26:04 +0530

    Migrated from SQLite3 to MySQL

--- PAAS_CRUD/PAAS_CRUD/settings.py
+++ PAAS_CRUD/PAAS_CRUD/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'PAAS_CRUD.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': r'D:\Nerdy Stuff\Python\Django\Comparision-Of-IAAS-PAAS-SAAS\my_sql_conf.cnf',
+        },
     }
 }
 
