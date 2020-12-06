4cadbf94b85b1d49c7e93075e1489e72da9c2bc3
Aniruddh M <animaruthesh@gmail.com>
2020-12-06 19:34:51 +0530

    Added results for Virtualization-SAAS and updated locustfile

--- Load_Testing/locustfile.py
+++ Load_Testing/locustfile.py
@@ -6,13 +6,36 @@ class UserBehaviour(TaskSet):
     #     # GET request
     #     self.client.get("/crud/Retrieve")
     #     print("Got a webpage response")
-    
+    @task
+    def post_Create(self):
+        # POST request on the CREATE page
+        response = self.client.get("/Create")
+        csrftoken = response.cookies['csrftoken']
+        self.client.post("/Create", {"USN": "1RV17EC011", "Branch": "CSE", "Name": "Jon Do"}, headers={"X-CSRFToken": csrftoken})
+        print("Posted a request")
+
+    @task
+    def post_update(self):
+        # POST request on the UPDATE page
+        response = self.client.get("/Update")
+        csrftoken = response.cookies['csrftoken']
+        self.client.post("/Update", {"USN": "1RV17EC011", "Branch": "CSE"}, headers={"X-CSRFToken": csrftoken})
+        print("Posted a request")
+
     @task
     def post_retrieve(self):
         # POST request on the RETRIEVE page
         response = self.client.get("/Retrieve")
         csrftoken = response.cookies['csrftoken']
         self.client.post("/Retrieve", {"USN": "1RV17EC011"}, headers={"X-CSRFToken": csrftoken})
+        print("Posted a request")
+
+    @task
+    def post_retrieve(self):
+        # POST request on the RETRIEVE page
+        response = self.client.get("/Delete")
+        csrftoken = response.cookies['csrftoken']
+        self.client.post("/Delete", {"USN": "1RV17EC011"}, headers={"X-CSRFToken": csrftoken})
         print("Posted a request")
 
 class User(HttpUser):
